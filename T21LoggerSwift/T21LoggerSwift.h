02ea4c45a7d3da720940c02e4e26bca592bfe02b
Patricia de la Rica Hernando <patricia.de@atos.net>
2019-05-14 13:14:28 +0200

    update framework

---
+++ T21LoggerSwift/T21LoggerSwift.h
@@ -0,0 +1,19 @@
+//
+//  T21LoggerSwift.h
+//  T21LoggerSwift
+//
+//  Created by Patri on 14/05/2019.
+//  Copyright © 2019 Worldline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for T21LoggerSwift.
+FOUNDATION_EXPORT double T21LoggerSwiftVersionNumber;
+
+//! Project version string for T21LoggerSwift.
+FOUNDATION_EXPORT const unsigned char T21LoggerSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <T21LoggerSwift/PublicHeader.h>
+
+
